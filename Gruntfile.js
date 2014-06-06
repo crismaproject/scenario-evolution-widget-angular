29733821d4caf36e3843f868bd9eb9d65f5b1f21
Daniel Meiers <daniel.meiers@cismet.de>
2014-06-06 15:11:07 +0200

    #2 removed coffe task from grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,14 +22,6 @@ module.exports = function (grunt) {
       dist: 'dist'
     },
     watch: {
-       coffee: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.coffee'],
-        tasks: ['coffee:dist']
-      },
-      coffeeTest: {
-        files: ['test/spec/{,*/}*.coffee'],
-        tasks: ['coffee:test']
-      },
       styles: {
         files: ['<%= yeoman.app %>/styles/{,*/}*.css'],
         tasks: ['copy:styles', 'autoprefixer']
@@ -119,30 +111,6 @@ module.exports = function (grunt) {
       all: [
         '<%= yeoman.app %>/scripts/{,*/}*.js'
       ]
-    },
-     coffee: {
-      options: {
-        sourceMap: true,
-        sourceRoot: ''
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/scripts',
-          src: '{,*/}*.coffee',
-          dest: '.tmp/scripts',
-          ext: '.js'
-        }]
-      },
-      test: {
-        files: [{
-          expand: true,
-          cwd: 'test/spec',
-          src: '{,*/}*.coffee',
-          dest: '.tmp/spec',
-          ext: '.js'
-        }]
-      }
     },
      // not used since Uglify task does concat,
     // but still available if needed
@@ -284,15 +252,12 @@ module.exports = function (grunt) {
     },
     concurrent: {
       server: [
-        'coffee:dist',
         'copy:styles'
       ],
       test: [
-        'coffee',
         'copy:styles'
       ],
       dist: [
-        'coffee',
         'copy:styles',
         'imagemin',
         'svgmin',
